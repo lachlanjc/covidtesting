4009e02d9ddc4c9435208482446f9c0c3daee571
Lachlan Campbell <lachlan@hackclub.com>
2020-03-20 05:49:41 -0400

    Inherit favicons

--- components/meta.js
+++ components/meta.js
@@ -19,5 +19,32 @@ export default ({
     {/* <meta property="og:image" content={image} /> */}
     {/* <meta name="twitter:card" content="summary_large_image" /> */}
     {/* <meta name="twitter:image" content={image} /> */}
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="https://predictcovid.com/apple-touch-icon.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="https://predictcovid.com/favicon-32x32.png"
+    />
+    <link
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="https://predictcovid.com/favicon-16x16.png"
+    />
+    <link rel="manifest" href="https://predictcovid.com/site.webmanifest" />
+    <link
+      rel="mask-icon"
+      href="https://predictcovid.com/safari-pinned-tab.svg"
+      color="#e55934"
+    />
+    <meta name="apple-mobile-web-app-title" content="COVID-19" />
+    <meta name="application-name" content="COVID-19" />
+    <meta name="msapplication-TileColor" content="#e55934" />
+    <meta name="theme-color" content="#e55934" />
   </Head>
 )
