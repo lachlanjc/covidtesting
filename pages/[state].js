2eacd1d07740f5aadbf7227652fc625b102c605e
Lachlan Campbell <lachlan@hackclub.com>
2020-03-23 02:41:30 -0400

    Add more stats & improve state page layout

--- pages/[state].js
+++ pages/[state].js
@@ -18,10 +18,10 @@ import { getJSON } from '../lib/util'
 import loadJSON from 'load-json-file'
 import Error from 'next/error'
 import MD from 'react-markdown'
-import { filter, find, orderBy } from 'lodash'
+import { filter, find, orderBy, round } from 'lodash'
 
 export default ({ errorCode, state, daily = [], latest = {}, info = {} }) => {
-  // if (errorCode) return <Error statusCode={errorCode} title="State not found" />
+  if (errorCode) return <Error statusCode={errorCode} title="State not found" />
   const [colorMode] = useColorMode()
   const accessory = {
     bg: colorMode === 'dark' ? null : 'rgba(255, 255, 255, 0.75)',
@@ -33,7 +33,7 @@ export default ({ errorCode, state, daily = [], latest = {}, info = {} }) => {
       <Header
         title={state.state}
         bgImg={state.landscape_background_url.replace('1280x720', '1920x1080')}
-        sx={{ textAlign: 'center', mb: [4, 5] }}
+        sx={{ textAlign: 'center' }}
       >
         <Flex sx={{ mt: [3, 4], justifyContent: 'center', flexWrap: 'wrap' }}>
           <Button
@@ -64,7 +64,7 @@ export default ({ errorCode, state, daily = [], latest = {}, info = {} }) => {
       </Header>
       <Container as="article" sx={{ maxWidth: 'copyPlus', pb: [4, 5, 6] }}>
         <Grid
-          columns={[null, null, '3fr 1fr 2fr']}
+          columns={[null, null, '3fr 2fr']}
           gap={[3, 4]}
           sx={{ mb: [4, 5], alignItems: 'end' }}
         >
@@ -72,17 +72,23 @@ export default ({ errorCode, state, daily = [], latest = {}, info = {} }) => {
             <Heading variant="headline" as="h2">
               Tests reported
             </Heading>
-            <Grid columns={2} sx={{}}>
-              <Stat value={latest.positive} label="Positive" lg />
-              <Stat value={latest.negative} label="Negative" lg />
-              <Stat value={latest.pending} label="Pending" lg />
-              <Stat value={latest.total} label="Total" lg />
+            <Grid columns={3}>
+              <Stat value={latest.total} label="Total tests reported" lg />
+              <Stat
+                value={round(latest.total / (state.population / 100000))}
+                label="Tests/100K population"
+                lg
+              />
+              <Stat
+                value={round(latest.positive / (state.population / 100000))}
+                label="Positive/100K population"
+                lg
+              />
+              <Stat value={latest.positive} label="Positive" />
+              <Stat value={latest.negative} label="Negative" />
+              <Stat value={latest.pending} label="Pending" />
             </Grid>
           </div>
-          <Grid columns={[2, 1]}>
-            <Stat value={state.population} label="Population" />
-            <Stat value={'#' + state.population_rank} label="Pop. rank" />
-          </Grid>
           <div>
             {info.notes && (
               <Card
@@ -119,6 +125,38 @@ export default ({ errorCode, state, daily = [], latest = {}, info = {} }) => {
           </div>
         </Grid>
         <StateChart data={orderBy(daily, 'date')} colorMode={colorMode} />
+        <Grid
+          columns={[null, '2fr 1fr']}
+          gap={[3, 4, 5]}
+          sx={{ mt: [4, null, null, 5] }}
+        >
+          <div>
+            <Heading variant="headline" as="h2">
+              Current stats
+            </Heading>
+            <Grid columns={[2, 4]}>
+              <Stat value={latest.total} label="Total cases" />
+              <Stat value={latest.hospitalized} label="Hospitalized" />
+              <Stat value={latest.death} label="Deaths" />
+              <Stat
+                value={new Date(latest.dateChecked)
+                  .toLocaleDateString()
+                  .replace('/2020', '')
+                  .replace('2020-', '')}
+                label="Last updated"
+              />
+            </Grid>
+          </div>
+          <div>
+            <Heading variant="headline" as="h2">
+              State info
+            </Heading>
+            <Grid columns={2}>
+              <Stat value={state.population} label="Population" />
+              <Stat value={'#' + state.population_rank} label="Pop. rank" />
+            </Grid>
+          </div>
+        </Grid>
       </Container>
     </>
   )
@@ -137,6 +175,6 @@ export const getServerSideProps = async req => {
   daily = filter(daily, { state: code })
   const latest = daily.length ? daily[0] : {}
   let info = await getJSON(`https://covidtracking.com/api/states/info`)
-  info = filter(info, { state: code })
+  info = find(info, { state: code })
   return { props: { state, daily, latest, info } }
 }
