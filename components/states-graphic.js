362d6f721e9cd86cc87836cd6d7bcad641c9b8e1
Lachlan Campbell <lachlan@hackclub.com>
2020-03-23 00:41:55 -0400

    Begin primary visualization

---
+++ components/states-graphic.js
@@ -0,0 +1,119 @@
+import React from 'react'
+import { geoCentroid } from 'd3-geo'
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Marker,
+  Annotation
+} from 'react-simple-maps'
+import { useThemeUI } from 'theme-ui'
+import { scaleQuantile } from 'd3-scale'
+import { map, find, round, kebabCase } from 'lodash'
+import Router from 'next/router'
+import allStates from '../public/states.json'
+
+const geoUrl = 'https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json'
+
+const offsets = {
+  VT: [50, -8],
+  NH: [34, 2],
+  MA: [30, -1],
+  RI: [28, 2],
+  CT: [35, 10],
+  NJ: [34, 1],
+  DE: [33, 0],
+  MD: [47, 10],
+  DC: [49, 21]
+}
+
+// prettier-ignore
+const colorRange = dark =>
+  dark
+    ? [ '#5e240d', '#6a290d', '#752f0d', '#7c3803', '#7e4e00', '#7e5c00',
+        '#7f6a00', '#7f7500', '#7d7f00', '#6f7f00', '#6a8000' ]
+    : [ '#b56c50', '#cd6644', '#e55934', '#f06529', '#f28b22', '#f2af23',
+        '#f2d323', '#f1e825', '#e5f127', '#d5f028', '#cff02b' ]
+
+const StatesGraphic = ({ data }) => {
+  const { theme, colorMode } = useThemeUI()
+  // http://www.colorbox.io/#steps=8#hue_start=17#hue_end=70#hue_curve=easeInOutSine#sat_start=28#sat_end=41#sat_curve=easeOutCubic#sat_rate=200#lum_start=71#lum_end=94#lum_curve=easeInExpo#lock_hex=e55934#minor_steps_map=0,30,40
+  const colorScale = scaleQuantile()
+    .domain(map(data, 'totalPC'))
+    .range(colorRange(colorMode === 'dark'))
+  return (
+    <ComposableMap projection="geoAlbersUsa">
+      <Geographies geography={geoUrl}>
+        {({ geographies }) => (
+          <>
+            {geographies.map(geo => {
+              const { name } = geo.properties
+              const cur = find(data, { name })
+              return (
+                <Geography
+                  key={geo.rsmKey}
+                  geography={geo}
+                  fill={cur ? colorScale(cur?.totalPC) : theme.colors.sheet}
+                  onClick={() => Router.push(`/${kebabCase(name)}`)}
+                  style={{ cursor: 'pointer' }}
+                />
+              )
+            })}
+            {geographies.map(geo => {
+              const centroid = geoCentroid(geo)
+              const cur = find(allStates, { name: geo.properties?.name })
+              if (!cur) return
+              const stateData = find(data, { name: geo.properties?.name })
+              const label = cur.abbrev || round(stateData.totalPC, 1)
+              return (
+                <g key={geo.rsmKey + '-flag'}>
+                  {cur &&
+                    centroid[0] > -160 &&
+                    centroid[0] < -67 &&
+                    (!Object.keys(offsets).includes(cur.abbrev) ? (
+                      <Marker coordinates={centroid}>
+                        <text
+                          y={2}
+                          fontSize={14}
+                          textAnchor="middle"
+                          fill={theme.colors.text}
+                        >
+                          {label}
+                        </text>
+                      </Marker>
+                    ) : (
+                      <Annotation
+                        subject={centroid}
+                        dx={offsets[cur?.abbrev]?.[0]}
+                        dy={offsets[cur?.abbrev]?.[1]}
+                        fill={theme.colors.muted}
+                      >
+                        <text
+                          x={4}
+                          fontSize={14}
+                          alignmentBaseline="middle"
+                          fill={theme.colors.text}
+                        >
+                          {label}
+                        </text>
+                      </Annotation>
+                    ))}
+                </g>
+              )
+            })}
+          </>
+        )}
+      </Geographies>
+      <style jsx>{`
+        .rsm-geography {
+          cursor: pointer;
+        }
+        text {
+          pointer-events: none;
+        }
+      `}</style>
+    </ComposableMap>
+  )
+}
+
+export default StatesGraphic
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Meta from '../components/meta'
 import Header from '../components/header'
+import StateGraphic from '../components/states-graphic'
 import StateList from '../components/state-list'
 import { Container, Heading, Text, Link } from 'theme-ui'
-// import { getJSON } from '../lib/util'
+import { getJSON } from '../lib/util'
+import loadJSON from 'load-json-file'
+import { find, map, pick } from 'lodash'
 
 export default ({ data = [], states = [] }) => {
   return (
@@ -10,7 +13,7 @@ export default ({ data = [], states = [] }) => {
       <Meta />
       <Header
         title="U.S. COVID-19 Testing"
-        desc="Across the U.S., inadequate COVID-19 testing is hampering our response to the pandemic."
+        desc="Across the country, inadequate COVID-19 testing is hampering our response to the pandemic."
       >
         <Text sx={{ color: 'secondary', mt: 3 }}>
           All data from{' '}
@@ -20,6 +23,9 @@ export default ({ data = [], states = [] }) => {
           .
         </Text>
       </Header>
+      <Container>
+        <StateGraphic data={data} states={states} />
+      </Container>
       <Container sx={{ maxWidth: [null, null, 'copyPlus'], py: [4, 5] }}>
         <Heading as="h2" variant="headline">
           Jump to a state
@@ -30,7 +36,24 @@ export default ({ data = [], states = [] }) => {
   )
 }
 
-// export const getServerSideProps = async () => {
-//   const data = await getJSON('https://covidtracking.com/api/states')
-//   return { props: { data } }
-// }
+export const getServerSideProps = async () => {
+  let states = await loadJSON('./public/states-full.json')
+  states = states.map(state => pick(state, ['code', 'state', 'population']))
+  let data = await getJSON('https://covidtracking.com/api/states')
+  const continental = map(states, 'code')
+  data = data.filter(({ state }) => continental.includes(state))
+  data = data.map(state => ({
+    name: find(states, ['code', state.state]).state,
+    pop: find(states, ['code', state.state]).population,
+    ...state
+  }))
+  data = data.map(({ name, positive, total, pop }) => ({
+    name,
+    pop,
+    positive,
+    total,
+    totalPC: total / (pop / 100000),
+    positivePC: positive / (pop / 100000)
+  }))
+  return { props: { data, states } }
+}
