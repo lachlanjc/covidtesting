02e62cae284acfa68becda1d22f4ccb561b7a293
Lachlan Campbell <lachlan@hackclub.com>
2020-03-25 14:54:21 -0400

    Fix analytics

--- pages/_document.js
+++ pages/_document.js
@@ -13,7 +13,7 @@ export default class extends Document {
         <Head>
           <link rel="stylesheet" href="https://predictcovid.com/fonts.css" />
           <style>{`:root { supported-color-schemes: light dark; }`}</style>
-          <script>{`window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};heap.load("935200349");`}</script>
+          <script dangerouslySetInnerHTML={{ __html: `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};heap.load("935200349");` }} />
         </Head>
         <body>
           <InitializeColorMode />
